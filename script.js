11a8bc95dc9b16769e9eda27c1b13d47f7efbc20
RomanRomaan <jay4or@gmail.com>
2025-07-21 14:52:02 +0300

    final

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const SliderImg = document.querySelector('.slider-img')
 const authorName = document.querySelector('.author-name')
 const aurhoeRole = document.querySelector('.author-role')
 const dots = document.querySelectorAll('.dot');
+const sliderContent = document.querySelector('.slider-content');
 
 
 let currentImg = 1
@@ -34,46 +35,57 @@ let sliderTextActive = {
 };
 
 function updateSliderItems() {
-    SliderImg.src = 'https://randomuser.me/api/portraits/men/' + currentImg + '.jpg'
-    sliderText.textContent = sliderTextActive[currentImg].text
-    authorName.textContent = sliderTextActive[currentImg].author
-    aurhoeRole.textContent = sliderTextActive[currentImg].position
+    sliderContent.classList.add('fade-out');
+    setTimeout(() => {
+        SliderImg.src = 'https://randomuser.me/api/portraits/men/' + currentImg + '.jpg';
+        sliderText.textContent = sliderTextActive[currentImg].text;
+        authorName.textContent = sliderTextActive[currentImg].author;
+        aurhoeRole.textContent = sliderTextActive[currentImg].position;
+        sliderContent.classList.remove('fade-out');
+    }, 300);
+}
+
+function updateArrows() {
+
+    if (currentImg <= 1) {
+        prevBtn.classList.add('disabled');
+    } else {
+        prevBtn.classList.remove('disabled');
+    }
+
+    if (currentImg >= 4) {
+        nextBtn.classList.add('disabled');
+    } else {
+        nextBtn.classList.remove('disabled');
+    }
+
 }
 function updateDots() {
     dots.forEach(dot => dot.classList.remove('active'));
     dots[currentImg - 1].classList.add('active');
 }
+
+function updateAll() {
+    updateSliderItems();
+    updateDots();
+    updateArrows();
+}
+
 dots.forEach((dot, idx) => {
     dot.addEventListener('click', () => {
         currentImg = idx + 1;
-        updateSliderItems();
-        updateDots();
+        updateAll();
     });
 });
-
 nextBtn.addEventListener('click', function () {
-    currentImg++
-    if (currentImg > 1) {
-        prevBtn.classList.remove('disabled')
-    }
-    if (currentImg >= 4) {
-        nextBtn.classList.toggle('disabled');
-        ;
-    }
-    updateSliderItems()
-    updateDots()
-
-});
-
-prevBtn.addEventListener('click', function () {
-    currentImg--
     if (currentImg < 4) {
-        nextBtn.classList.remove('disabled')
+        currentImg++;
+        updateAll();
     }
-    if (currentImg <= 1) {
-        prevBtn.classList.toggle('disabled');
+});
+prevBtn.addEventListener('click', function () {
+    if (currentImg > 1) {
+        currentImg--;
+        updateAll();
     }
-    updateSliderItems()
-    updateDots()
-
 });
